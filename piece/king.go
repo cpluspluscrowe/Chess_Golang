c96b9641807085bc147ed47d6096d411318a0a2a
Chad Crowe <ccrowe@linkedin.com>
2018-08-10 20:17:06 -0500

    added king test file

--- piece/king.go
+++ piece/king.go
@@ -15,20 +15,21 @@ func (p King) String() string {
 	return fmt.Sprintf("{color:%s, movement:[%d, %d]}", color, p.Position.Y, p.Position.X)
 }
 
-func (king King) AddPosition(x int, y int) {
+func (king *King) AddPosition(x int, y int) {
 	king.Moves = append(king.Moves, movement.Position{x,y})
 }
 
 func NewKing(position movement.Position) *King {
 	king := &King{}
 	king.Position = position
+	king.Moves = []movement.Position{}
 	king.AddPosition(-1,-1)
-	king.AddPosition(0,-1)
 	king.AddPosition(0,-1)
 	king.AddPosition(1,-1)
 	king.AddPosition(1,0)
 	king.AddPosition(1,1)
 	king.AddPosition(0,1)
 	king.AddPosition(-1,1)
+	king.AddPosition(-1,0)
 	return king
 }
