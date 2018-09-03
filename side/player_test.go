96f4cdddcf86811e56887d04a24bac34bed4ad0b
Chad Crowe <ccrowe@linkedin.com>
2018-09-03 10:15:36 -0500

    moving tests and deleting main test file

--- side/player_test.go
+++ side/player_test.go
@@ -4,7 +4,8 @@ import (
 	"testing"
 	"Chess/color"
 	"Chess/movement"
-		)
+	"fmt"
+)
 
 func TestVerifyThatOldPositionIsOccupied(t *testing.T){
 	black := NewPlayer(color.NewColor(true))
@@ -14,5 +15,42 @@ func TestVerifyThatOldPositionIsOccupied(t *testing.T){
 	returned := black.addMovePotentialPositions(blackKing, &potentialPositions)
 	if len(returned) != 3 {
 		t.Errorf("Should return three positions")
+	}
+}
+
+func TestThatPositionIsOccupiedAfterAddingPiece(t *testing.T){
+	player := NewPlayer(color.Color{false})
+	player.AddKing(movement.Position{1,1}, false)
+	ok, _ := player.OccupiedPositions[movement.Position{1,1}]
+	if ok {
+		fmt.Errorf("movement should be occupied")
+	}
+}
+
+func TestValidMove(t *testing.T) {
+	player := NewPlayer(color.Color{false})
+	valid := player.IsMoveValid(movement.Position{0,8})
+	if valid {
+		t.Errorf("Move is not valid, but function returned valid")
+	}
+	valid = player.IsMoveValid(movement.Position{8,0})
+	if valid {
+		t.Errorf("Move is not valid, but function returned valid")
+	}
+	valid = player.IsMoveValid(movement.Position{0,-1})
+	if valid {
+		t.Errorf("Move is not valid, but function returned valid")
+	}
+	valid = player.IsMoveValid(movement.Position{-1,0})
+	if valid {
+		t.Errorf("Move is not valid, but function returned valid")
+	}
+	valid = player.IsMoveValid(movement.Position{7,7})
+	if !valid {
+		t.Errorf("Move is valid, but function returned not valid")
+	}
+	valid = player.IsMoveValid(movement.Position{0,0})
+	if !valid {
+		t.Errorf("Move is valid, but function returned not valid")
 	}
 }
